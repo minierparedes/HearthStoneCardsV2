f5bb73c5620703c354b2d95753fa0db4d6e91df9
minierparedes <3thancr0wn@gmail.com>
2021-03-09 11:46:19 +0900

    Initial Commit

---
+++ HearthStone/HearthStoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearthStoneApp.swift
+//  HearthStone
+//
+//  Created by ethancr0wn on 2021/03/09.
+//
+
+import SwiftUI
+
+@main
+struct HearthStoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
